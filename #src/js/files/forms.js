aaa38a2b620aa594c92b7d52953187c41b2aac41
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2021-09-26 18:29:16 +0300

    ab

--- #src/js/files/forms.js
+++ #src/js/files/forms.js
@@ -52,6 +52,7 @@ async function form_submit(e) {
 		e.preventDefault();
 	}
 }
+
 function form_validate(form) {
 	let error = 0;
 	let form_req = form.querySelectorAll('._req');
@@ -65,6 +66,7 @@ function form_validate(form) {
 	}
 	return error;
 }
+
 function form_validate_input(input) {
 	let error = 0;
 	let input_g_value = input.getAttribute('data-value');
@@ -93,6 +95,7 @@ function form_validate_input(input) {
 	}
 	return error;
 }
+
 function form_add_error(input) {
 	input.classList.add('_error');
 	input.parentElement.classList.add('_error');
@@ -106,6 +109,7 @@ function form_add_error(input) {
 		input.parentElement.insertAdjacentHTML('beforeend', '<div class="form__error">' + input_error_text + '</div>');
 	}
 }
+
 function form_remove_error(input) {
 	input.classList.remove('_error');
 	input.parentElement.classList.remove('_error');
@@ -115,6 +119,7 @@ function form_remove_error(input) {
 		input.parentElement.removeChild(input_error);
 	}
 }
+
 function form_clean(form) {
 	let inputs = form.querySelectorAll('input,textarea');
 	for (let index = 0; index < inputs.length; index++) {
@@ -152,196 +157,6 @@ for (let index = 0; index < viewPass.length; index++) {
 		}
 		element.classList.toggle('_active');
 	});
-}
-
-//Select
-let selects = document.getElementsByTagName('select');
-if (selects.length > 0) {
-	selects_init();
-}
-function selects_init() {
-	for (let index = 0; index < selects.length; index++) {
-		const select = selects[index];
-		select_init(select);
-	}
-	//select_callback();
-	document.addEventListener('click', function (e) {
-		selects_close(e);
-	});
-	document.addEventListener('keydown', function (e) {
-		if (e.code === 'Escape') {
-			selects_close(e);
-		}
-	});
-}
-function selects_close(e) {
-	const selects = document.querySelectorAll('.select');
-	if (!e.target.closest('.select') && !e.target.classList.contains('_option')) {
-		for (let index = 0; index < selects.length; index++) {
-			const select = selects[index];
-			const select_body_options = select.querySelector('.select__options');
-			select.classList.remove('_active');
-			_slideUp(select_body_options, 100);
-		}
-	}
-}
-function select_init(select) {
-	const select_parent = select.parentElement;
-	const select_modifikator = select.getAttribute('class');
-	const select_selected_option = select.querySelector('option:checked');
-	select.setAttribute('data-default', select_selected_option.value);
-	select.style.display = 'none';
-
-	select_parent.insertAdjacentHTML('beforeend', '<div class="select select_' + select_modifikator + '"></div>');
-
-	let new_select = select.parentElement.querySelector('.select');
-	new_select.appendChild(select);
-	select_item(select);
-}
-function select_item(select) {
-	const select_parent = select.parentElement;
-	const select_items = select_parent.querySelector('.select__item');
-	const select_options = select.querySelectorAll('option');
-	const select_selected_option = select.querySelector('option:checked');
-	const select_selected_text = select_selected_option.text;
-	const select_type = select.getAttribute('data-type');
-
-	if (select_items) {
-		select_items.remove();
-	}
-
-	let select_type_content = '';
-	if (select_type == 'input') {
-		select_type_content = '<div class="select__value icon-select-arrow"><input autocomplete="off" type="text" name="form[]" value="' + select_selected_text + '" data-error="Ошибка" data-value="' + select_selected_text + '" class="select__input"></div>';
-	} else {
-		select_type_content = '<div class="select__value icon-select-arrow"><span>' + select_selected_text + '</span></div>';
-	}
-
-	select_parent.insertAdjacentHTML('beforeend',
-		'<div class="select__item">' +
-		'<div class="select__title">' + select_type_content + '</div>' +
-		'<div class="select__options">' + select_get_options(select_options) + '</div>' +
-		'</div></div>');
-
-	select_actions(select, select_parent);
-}
-function select_actions(original, select) {
-	const select_item = select.querySelector('.select__item');
-	const selectTitle = select.querySelector('.select__title');
-	const select_body_options = select.querySelector('.select__options');
-	const select_options = select.querySelectorAll('.select__option');
-	const select_type = original.getAttribute('data-type');
-	const select_input = select.querySelector('.select__input');
-
-	selectTitle.addEventListener('click', function (e) {
-		selectItemActions();
-	});
-
-	function selectMultiItems() {
-		let selectedOptions = select.querySelectorAll('.select__option');
-		let originalOptions = original.querySelectorAll('option');
-		let selectedOptionsText = [];
-		for (let index = 0; index < selectedOptions.length; index++) {
-			const selectedOption = selectedOptions[index];
-			originalOptions[index].removeAttribute('selected');
-			if (selectedOption.classList.contains('_selected')) {
-				const selectOptionText = selectedOption.innerHTML;
-				selectedOptionsText.push(selectOptionText);
-				originalOptions[index].setAttribute('selected', 'selected');
-			}
-		}
-		select.querySelector('.select__value').innerHTML = '<span>' + selectedOptionsText + '</span>';
-	}
-	function selectItemActions(type) {
-		if (!type) {
-			let selects = document.querySelectorAll('.select');
-			for (let index = 0; index < selects.length; index++) {
-				const select = selects[index];
-				const select_body_options = select.querySelector('.select__options');
-				if (select != select_item.closest('.select')) {
-					select.classList.remove('_active');
-					_slideUp(select_body_options, 100);
-				}
-			}
-			_slideToggle(select_body_options, 100);
-			select.classList.toggle('_active');
-		}
-	}
-	for (let index = 0; index < select_options.length; index++) {
-		const select_option = select_options[index];
-		const select_option_value = select_option.getAttribute('data-value');
-		const select_option_text = select_option.innerHTML;
-
-		if (select_type == 'input') {
-			select_input.addEventListener('keyup', select_search);
-		} else {
-			if (select_option.getAttribute('data-value') == original.value && !original.hasAttribute('multiple')) {
-				select_option.style.display = 'none';
-			}
-		}
-		select_option.addEventListener('click', function () {
-			for (let index = 0; index < select_options.length; index++) {
-				const el = select_options[index];
-				el.style.display = 'block';
-			}
-			if (select_type == 'input') {
-				select_input.value = select_option_text;
-				original.value = select_option_value;
-			} else {
-				if (original.hasAttribute('multiple')) {
-					select_option.classList.toggle('_selected');
-					selectMultiItems();
-				} else {
-					select.querySelector('.select__value').innerHTML = '<span>' + select_option_text + '</span>';
-					original.value = select_option_value;
-					select_option.style.display = 'none';
-				}
-			}
-			let type;
-			if (original.hasAttribute('multiple')) {
-				type = 'multiple';
-			}
-			selectItemActions(type);
-		});
-	}
-}
-function select_get_options(select_options) {
-	if (select_options) {
-		let select_options_content = '';
-		for (let index = 0; index < select_options.length; index++) {
-			const select_option = select_options[index];
-			const select_option_value = select_option.value;
-			if (select_option_value != '') {
-				const select_option_text = select_option.innerHTML;
-				select_options_content = select_options_content + '<div data-value="' + select_option_value + '" class="select__option">' + select_option_text + '</div>';
-			}
-		}
-		return select_options_content;
-	}
-}
-function select_search(e) {
-	let select_block = e.target.closest('.select ').querySelector('.select__options');
-	let select_options = e.target.closest('.select ').querySelectorAll('.select__option');
-	let select_search_text = e.target.value.toUpperCase();
-
-	for (let i = 0; i < select_options.length; i++) {
-		let select_option = select_options[i];
-		let select_txt_value = select_option.textContent || select_option.innerText;
-		if (select_txt_value.toUpperCase().indexOf(select_search_text) > -1) {
-			select_option.style.display = "";
-		} else {
-			select_option.style.display = "none";
-		}
-	}
-}
-function selects_update_all() {
-	let selects = document.querySelectorAll('select');
-	if (selects) {
-		for (let index = 0; index < selects.length; index++) {
-			const select = selects[index];
-			select_item(select);
-		}
-	}
 }
 
 //Placeholers
@@ -444,79 +259,26 @@ function inputs_init(inputs) {
 		}
 	}
 }
+
 function input_placeholder_add(input) {
 	const input_g_value = input.getAttribute('data-value');
 	if (input.value == '' && input_g_value != '') {
 		input.value = input_g_value;
 	}
 }
+
 function input_focus_add(input) {
 	input.classList.add('_focus');
 	input.parentElement.classList.add('_focus');
 }
+
 function input_focus_remove(input) {
 	input.classList.remove('_focus');
 	input.parentElement.classList.remove('_focus');
 }
+
 function input_clear_mask(input, input_g_value) {
 	input.inputmask.remove();
 	input.value = input_g_value;
 	input_focus_remove(input);
-}
-
-//QUANTITY
-let quantityButtons = document.querySelectorAll('.quantity__button');
-if (quantityButtons.length > 0) {
-	for (let index = 0; index < quantityButtons.length; index++) {
-		const quantityButton = quantityButtons[index];
-		quantityButton.addEventListener("click", function (e) {
-			let value = parseInt(quantityButton.closest('.quantity').querySelector('input').value);
-			if (quantityButton.classList.contains('quantity__button_plus')) {
-				value++;
-			} else {
-				value = value - 1;
-				if (value < 1) {
-					value = 1
-				}
-			}
-			quantityButton.closest('.quantity').querySelector('input').value = value;
-		});
-	}
-}
-
-//RANGE
-const priceSlider = document.querySelector('.price-filter__slider');
-if (priceSlider) {
-
-	let textFrom = priceSlider.getAttribute('data-from');
-	let textTo = priceSlider.getAttribute('data-to');
-
-	noUiSlider.create(priceSlider, {
-		start: [0, 200000],
-		connect: true,
-		tooltips: [wNumb({ decimals: 0, prefix: textFrom + ' ' }), wNumb({ decimals: 0, prefix: textTo + ' ' })],
-		range: {
-			'min': [0],
-			'max': [200000]
-		}
-	});
-
-	/*
-	const priceStart = document.getElementById('price-start');
-	const priceEnd = document.getElementById('price-end');
-	priceStart.addEventListener('change', setPriceValues);
-	priceEnd.addEventListener('change', setPriceValues);
-	*/
-
-	function setPriceValues() {
-		let priceStartValue;
-		let priceEndValue;
-		if (priceStart.value != '') {
-			priceStartValue = priceStart.value;
-		}
-		if (priceEnd.value != '') {
-			priceEndValue = priceEnd.value;
-		}
-		priceSlider.noUiSlider.set([priceStartValue, priceEndValue]);
-	}
 }
--- #src/js/files/functions.js
+++ #src/js/files/functions.js
@@ -185,72 +185,8 @@ for (let index = 0; index < tabs.length; index++) {
 	}
 }
 
-
 //=================
-//Spollers
-let spollers = document.querySelectorAll("._spoller");
-let spollersGo = true;
-if (spollers.length > 0) {
 
-	function spollerCLick(e) {
-		const spoller = e.target;
-		if (spollersGo) {
-			spollersGo = false;
-
-			if (spoller.closest('._spollers').classList.contains('_one')) {
-				let curent_spollers = spoller.closest('._spollers').querySelectorAll('._spoller');
-				for (let i = 0; i < curent_spollers.length; i++) {
-					let el = curent_spollers[i];
-					if (el != spoller) {
-						el.classList.remove('_active');
-						_slideUp(el.nextElementSibling);
-					}
-				}
-			}
-			spoller.classList.toggle('_active');
-			_slideToggle(spoller.nextElementSibling);
-
-			setTimeout(function () {
-				spollersGo = true;
-			}, 500);
-		}
-	}
-
-	function spollersInit() {
-		for (let index = 0; index < spollers.length; index++) {
-			const spoller = spollers[index];
-			let spollerMax = spoller.getAttribute('data-max');
-
-			if (spollerMax && window.innerWidth > spollerMax) {
-				if (spoller.classList.contains('_init')) {
-					spoller.classList.remove('_active');
-					spoller.classList.remove('_init');
-					spoller.nextElementSibling.style.cssText = '';
-					spoller.removeEventListener("click", spollerCLick);
-				}
-			} else if (!spoller.classList.contains('_init')) {
-				spoller.classList.add('_init');
-				spoller.addEventListener("click", spollerCLick);
-			}
-		}
-	}
-
-	function spollersShowActive() {
-		for (let index = 0; index < spollers.length; index++) {
-			const spoller = spollers[index];
-			if (spoller.classList.contains('_active')) {
-				_slideToggle(spoller.nextElementSibling);
-			}
-		}
-	}
-	window.addEventListener("resize", spollersInit);
-
-	setTimeout(function () {
-		spollersShowActive();
-		spollersInit();
-	}, 0);
-}
-//=================
 //Gallery
 let gallery = document.querySelectorAll('._gallery');
 if (gallery) {
